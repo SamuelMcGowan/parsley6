dce9397880b00b0c4e592f79de0509137191863d
Sam McGowan <the.sam.mcg@protonmail.com>
2024-10-26 14:41:23 +0100

    Update stream trait.

--- src/stream.rs
+++ src/stream.rs
@@ -2,18 +2,25 @@ use std::{marker::PhantomData, slice::Iter, str::Chars};
 
 pub trait Stream<'a> {
     type Token: Clone + PartialEq;
-    type Slice: Slice + PartialEq + ?Sized;
+    type Slice: PartialEq + ?Sized + 'a;
 
     fn peek_token(&self) -> Option<Self::Token>;
 
     #[must_use = "If you don't need the token, use `Input::advance`"]
     fn next_token(&mut self) -> Option<Self::Token>;
 
-    fn as_slice(&self) -> &'a Self::Slice;
+    fn peek_slice(&self, len: usize) -> Option<&'a Self::Slice>;
+
+    #[must_use = "If you don't need the slice, use `Input::advance_len`"]
+    fn next_slice(&mut self, len: usize) -> Option<&'a Self::Slice>;
 
     #[inline]
     fn advance(&mut self) {
         let _ = self.next_token();
+    }
+
+    fn advance_len(&mut self, len: usize) {
+        let _ = self.next_slice(len);
     }
 
     #[inline]
@@ -22,38 +29,9 @@ pub trait Stream<'a> {
     }
 }
 
-/// A slice, for example `[T]` or [`str`].
-pub trait Slice {
-    fn try_prefix(&self, len: usize) -> Option<&Self>;
-
-    #[inline]
-    fn prefix(&self, len: usize) -> &Self {
-        self.try_prefix(len).expect("len out of range")
-    }
-}
-
-impl Slice for str {
-    #[inline]
-    fn try_prefix(&self, len: usize) -> Option<&Self> {
-        self.get(..len)
-    }
-}
-
-impl<T> Slice for [T] {
-    #[inline]
-    fn try_prefix(&self, len: usize) -> Option<&Self> {
-        self.get(..len)
-    }
-}
-
 impl<'a> Stream<'a> for Chars<'a> {
     type Token = char;
     type Slice = str;
-
-    #[inline]
-    fn as_slice(&self) -> &'a Self::Slice {
-        self.as_str()
-    }
 
     #[inline]
     fn peek_token(&self) -> Option<Self::Token> {
@@ -63,6 +41,18 @@ impl<'a> Stream<'a> for Chars<'a> {
     #[inline]
     fn next_token(&mut self) -> Option<Self::Token> {
         self.next()
+    }
+
+    #[inline]
+    fn peek_slice(&self, len: usize) -> Option<&'a Self::Slice> {
+        self.as_str().split_at_checked(len).map(|(slice, _)| slice)
+    }
+
+    #[inline]
+    fn next_slice(&mut self, len: usize) -> Option<&'a Self::Slice> {
+        let (slice, rest) = self.as_str().split_at_checked(len)?;
+        *self = rest.chars();
+        Some(slice)
     }
 }
 
@@ -71,11 +61,6 @@ impl<'a, T: Clone + PartialEq> Stream<'a> for Iter<'a, T> {
     type Slice = [T];
 
     #[inline]
-    fn as_slice(&self) -> &'a Self::Slice {
-        self.as_slice()
-    }
-
-    #[inline]
     fn peek_token(&self) -> Option<Self::Token> {
         self.clone().next()
     }
@@ -83,6 +68,20 @@ impl<'a, T: Clone + PartialEq> Stream<'a> for Iter<'a, T> {
     #[inline]
     fn next_token(&mut self) -> Option<Self::Token> {
         self.next()
+    }
+
+    #[inline]
+    fn peek_slice(&self, len: usize) -> Option<&'a Self::Slice> {
+        self.as_slice()
+            .split_at_checked(len)
+            .map(|(slice, _)| slice)
+    }
+
+    #[inline]
+    fn next_slice(&mut self, len: usize) -> Option<&'a Self::Slice> {
+        let (slice, rest) = self.as_slice().split_at_checked(len)?;
+        *self = rest.iter();
+        Some(slice)
     }
 }
 
@@ -118,13 +117,23 @@ impl<'a, S: Stream<'a>, State> Stream<'a> for StreamWithState<'a, S, State> {
     }
 
     #[inline]
-    fn as_slice(&self) -> &'a Self::Slice {
-        self.stream.as_slice()
+    fn peek_slice(&self, len: usize) -> Option<&'a Self::Slice> {
+        self.stream.peek_slice(len)
+    }
+
+    #[inline]
+    fn next_slice(&mut self, len: usize) -> Option<&'a Self::Slice> {
+        self.stream.next_slice(len)
     }
 
     #[inline]
     fn advance(&mut self) {
         self.stream.advance();
+    }
+
+    #[inline]
+    fn advance_len(&mut self, len: usize) {
+        self.stream.advance_len(len);
     }
 
     #[inline]
