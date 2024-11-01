9568fb9dbeb031c3846207bac07315fb4f9f7479
Sam McGowan <the.sam.mcg@protonmail.com>
2024-11-01 16:24:49 +0000

    Add missing bounds to token parsers for better error messages.

--- src/token.rs
+++ src/token.rs
@@ -1,6 +1,8 @@
-// TODO: add more bounds to builder functions.
-
 pub mod text;
+
+use std::marker::PhantomData;
+
+use derive_where::derive_where;
 
 use crate::error::{Cause, Error};
 use crate::parser::Parser;
@@ -8,14 +10,24 @@ use crate::prelude::TokenSet;
 use crate::stream::{Stream, StreamEatSlice};
 
 #[inline]
-pub fn peek<T>(token: T) -> Peek<T> {
-    Peek(token)
+pub fn peek<S, E>(token: S::Token) -> Peek<S, E>
+where
+    S: Stream,
+    E: Error<S>,
+{
+    Peek {
+        token,
+        _phantom: PhantomData,
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Peek<T>(T);
+#[derive_where(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash; S::Token)]
+pub struct Peek<S: Stream, E> {
+    token: S::Token,
+    _phantom: PhantomData<*const E>,
+}
 
-impl<S, E> Parser<S, S::Token, E> for Peek<S::Token>
+impl<S, E> Parser<S, S::Token, E> for Peek<S, E>
 where
     S: Stream,
     E: Error<S>,
@@ -23,9 +35,9 @@ where
     #[inline]
     fn parse(&mut self, stream: &mut S) -> Result<S::Token, E> {
         match stream.peek_token() {
-            Some(token) if token == self.0 => Ok(token),
+            Some(token) if token == self.token => Ok(token),
             _ => Err(E::new(
-                Cause::ExpectedToken(self.0.clone()),
+                Cause::ExpectedToken(self.token.clone()),
                 stream.peek_token_span(),
             )),
         }
@@ -33,14 +45,24 @@ where
 }
 
 #[inline]
-pub fn eat<T>(token: T) -> Eat<T> {
-    Eat(token)
+pub fn eat<S, E>(token: S::Token) -> Eat<S, E>
+where
+    S: Stream,
+    E: Error<S>,
+{
+    Eat {
+        token,
+        _phantom: PhantomData,
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Eat<T>(T);
+#[derive_where(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash; S::Token)]
+pub struct Eat<S: Stream, E> {
+    token: S::Token,
+    _phantom: PhantomData<*const E>,
+}
 
-impl<S, E> Parser<S, S::Token, E> for Eat<S::Token>
+impl<S, E> Parser<S, S::Token, E> for Eat<S, E>
 where
     S: Stream,
     E: Error<S>,
@@ -48,12 +70,12 @@ where
     #[inline]
     fn parse(&mut self, stream: &mut S) -> Result<S::Token, E> {
         match stream.peek_token() {
-            Some(token) if token == self.0 => {
+            Some(token) if token == self.token => {
                 stream.next_token();
                 Ok(token)
             }
             _ => Err(E::new(
-                Cause::ExpectedToken(self.0.clone()),
+                Cause::ExpectedToken(self.token.clone()),
                 stream.peek_token_span(),
             )),
         }
@@ -61,14 +83,25 @@ where
 }
 
 #[inline]
-pub fn peek_in<T>(token_set: T) -> PeekIn<T> {
-    PeekIn(token_set)
+pub fn peek_in<T, S, E>(token_set: T) -> PeekIn<T, S, E>
+where
+    T: TokenSet<S::Token>,
+    S: Stream,
+    E: Error<S>,
+{
+    PeekIn {
+        token_set,
+        _phantom: PhantomData,
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct PeekIn<T>(T);
+#[derive_where(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash; T)]
+pub struct PeekIn<T, S, E> {
+    token_set: T,
+    _phantom: PhantomData<*const (S, E)>,
+}
 
-impl<T, S, E> Parser<S, S::Token, E> for PeekIn<T>
+impl<T, S, E> Parser<S, S::Token, E> for PeekIn<T, S, E>
 where
     T: TokenSet<S::Token>,
     S: Stream,
@@ -77,21 +110,32 @@ where
     #[inline]
     fn parse(&mut self, stream: &mut S) -> Result<S::Token, E> {
         match stream.peek_token() {
-            Some(token) if self.0.contains(&token) => Ok(token),
+            Some(token) if self.token_set.contains(&token) => Ok(token),
             _ => Err(E::new(Cause::ExpectedInSet, stream.peek_token_span())),
         }
     }
 }
 
 #[inline]
-pub fn eat_in<T>(token_set: T) -> EatIn<T> {
-    EatIn(token_set)
+pub fn eat_in<T, S, E>(token_set: T) -> EatIn<T, S, E>
+where
+    T: TokenSet<S::Token>,
+    S: Stream,
+    E: Error<S>,
+{
+    EatIn {
+        token_set,
+        _phantom: PhantomData,
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct EatIn<T>(T);
+#[derive_where(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash; T)]
+pub struct EatIn<T, S, E> {
+    token_set: T,
+    _phantom: PhantomData<*const (S, E)>,
+}
 
-impl<T, S, E> Parser<S, S::Token, E> for EatIn<T>
+impl<T, S, E> Parser<S, S::Token, E> for EatIn<T, S, E>
 where
     T: TokenSet<S::Token>,
     S: Stream,
@@ -100,7 +144,7 @@ where
     #[inline]
     fn parse(&mut self, stream: &mut S) -> Result<S::Token, E> {
         match stream.peek_token() {
-            Some(token) if self.0.contains(&token) => {
+            Some(token) if self.token_set.contains(&token) => {
                 stream.next_token();
                 Ok(token)
             }
@@ -110,14 +154,25 @@ where
 }
 
 #[inline]
-pub fn peek_slice<Slice: ?Sized>(slice: &Slice) -> PeekSlice<Slice> {
-    PeekSlice(slice)
+pub fn peek_slice<Slice, S, E>(slice: &Slice) -> PeekSlice<Slice, S, E>
+where
+    Slice: ?Sized,
+    S: StreamEatSlice<Slice>,
+    E: Error<S>,
+{
+    PeekSlice {
+        slice,
+        _phantom: PhantomData,
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct PeekSlice<'a, Slice: ?Sized>(&'a Slice);
+#[derive_where(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash; &'a Slice)]
+pub struct PeekSlice<'a, Slice: ?Sized, S, E> {
+    slice: &'a Slice,
+    _phantom: PhantomData<*const (S, E)>,
+}
 
-impl<'a, Slice, S, E> Parser<S, S::SliceRef, E> for PeekSlice<'a, Slice>
+impl<'a, Slice, S, E> Parser<S, S::SliceRef, E> for PeekSlice<'a, Slice, S, E>
 where
     Slice: ?Sized,
     S: StreamEatSlice<Slice>,
@@ -126,20 +181,31 @@ where
     #[inline]
     fn parse(&mut self, stream: &mut S) -> Result<S::SliceRef, E> {
         stream
-            .peek_slice(self.0)
+            .peek_slice(self.slice)
             .ok_or_else(|| E::new(Cause::ExpectedSlice, stream.peek_token_span()))
     }
 }
 
 #[inline]
-pub fn eat_slice<Slice: ?Sized>(slice: &Slice) -> EatSlice<Slice> {
-    EatSlice(slice)
+pub fn eat_slice<Slice, S, E>(slice: &Slice) -> EatSlice<Slice, S, E>
+where
+    Slice: ?Sized,
+    S: StreamEatSlice<Slice>,
+    E: Error<S>,
+{
+    EatSlice {
+        slice,
+        _phantom: PhantomData,
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct EatSlice<'a, Slice: ?Sized>(&'a Slice);
+#[derive_where(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash; &'a Slice)]
+pub struct EatSlice<'a, Slice: ?Sized, S, E> {
+    slice: &'a Slice,
+    _phantom: PhantomData<*const (S, E)>,
+}
 
-impl<'a, Slice, S, E> Parser<S, S::SliceRef, E> for EatSlice<'a, Slice>
+impl<'a, Slice, S, E> Parser<S, S::SliceRef, E> for EatSlice<'a, Slice, S, E>
 where
     Slice: ?Sized,
     S: StreamEatSlice<Slice>,
@@ -148,21 +214,29 @@ where
     #[inline]
     fn parse(&mut self, stream: &mut S) -> Result<S::SliceRef, E> {
         stream
-            .eat_slice(self.0)
+            .eat_slice(self.slice)
             .ok_or_else(|| E::new(Cause::ExpectedSlice, stream.peek_token_span()))
     }
 }
 
 #[inline]
-pub fn end() -> End {
-    End {}
+pub fn end<S, E>() -> End<S, E>
+where
+    S: Stream,
+    E: Error<S>,
+{
+    End {
+        _phantom: PhantomData,
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive_where(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[non_exhaustive]
-pub struct End {}
+pub struct End<S, E> {
+    _phantom: PhantomData<*const (S, E)>,
+}
 
-impl<S, E> Parser<S, (), E> for End
+impl<S, E> Parser<S, (), E> for End<S, E>
 where
     S: Stream,
     E: Error<S>,
@@ -178,16 +252,25 @@ where
 }
 
 #[inline]
-pub fn eat_while_in<T>(token_set: T) -> EatWhileIn<T> {
-    EatWhileIn { token_set }
+pub fn eat_while_in<T, S, E>(token_set: T) -> EatWhileIn<T, S, E>
+where
+    T: TokenSet<S::Token>,
+    S: Stream,
+    E: Error<S>,
+{
+    EatWhileIn {
+        token_set,
+        _phantom: PhantomData,
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct EatWhileIn<T> {
-    pub(crate) token_set: T,
+#[derive_where(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash; T)]
+pub struct EatWhileIn<T, S, E> {
+    token_set: T,
+    _phantom: PhantomData<*const (S, E)>,
 }
 
-impl<T, S, E> Parser<S, S::SliceRef, E> for EatWhileIn<T>
+impl<T, S, E> Parser<S, S::SliceRef, E> for EatWhileIn<T, S, E>
 where
     T: TokenSet<S::Token>,
     S: Stream,
